aefb40d587bbca772111d0b3444cfec371defba7
2unmi <2unmin4030@gmail.com>
2025-02-27 06:23:51 +0900

    add : 이미지 확장자 및 파일 크기 체크 로직 추가

--- src/main/java/com/example/community_feed/image/ImageService.java
+++ src/main/java/com/example/community_feed/image/ImageService.java
@@ -8,16 +8,23 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 @Service
 @RequiredArgsConstructor
 public class ImageService {
+
     private final AmazonS3 amazonS3;
-    // todo 파일 확장자, 파일 크기 설정
+
     @Value("${aws.bucket}")
     private String bucket;
 
+    private static final String[] PERMITTED_FILE_EXTENSIONS = {"jpg", "png"};
+
     public String upload(MultipartFile multipartFile) {
+        if (!isValid(multipartFile)) {
+            throw new RuntimeException("올바른 파일이 아닙니다");
+        }
         String originalFilename = multipartFile.getOriginalFilename();
 
         ObjectMetadata objectMetadata = new ObjectMetadata();
@@ -31,5 +38,18 @@ public class ImageService {
         }
         return amazonS3.getUrl(bucket, originalFilename).toString();
 
+    }
+
+    private boolean isExtension(String name) {
+        String extension = name.substring(name.lastIndexOf('.') + 1).toLowerCase();
+        return Arrays.asList(PERMITTED_FILE_EXTENSIONS).contains(extension);
+    }
+
+    private boolean isValid(MultipartFile file) {
+        if (!isExtension(file.getOriginalFilename()) || file.getSize() > 5 * 1024 * 1024) {
+            return false;
+        }
+
+        return true;
     }
 }
