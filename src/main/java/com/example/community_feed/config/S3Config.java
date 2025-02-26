568e3d42d3f23a1740bbba4fc37ac1829b1b7331
2unmi <2unmin4030@gmail.com>
2025-02-27 05:49:12 +0900

    fix : 이미지 컨트롤러 생성 게시물에 이미지 업로드기능을 이미지 컨트롤러와 서비스로 분리

--- src/main/java/com/example/community_feed/config/S3Config.java
+++ src/main/java/com/example/community_feed/config/S3Config.java
@@ -3,7 +3,7 @@ package com.example.community_feed.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,9 +19,9 @@ public class S3Config {
     private String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
+    public AmazonS3 amazonS3() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder
+        return AmazonS3ClientBuilder
                 .standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
--- src/main/java/com/example/community_feed/image/ImageService.java
+++ src/main/java/com/example/community_feed/image/ImageService.java
@@ -1,6 +1,6 @@
 package com.example.community_feed.image;
 
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,20 +12,24 @@ import java.io.IOException;
 @Service
 @RequiredArgsConstructor
 public class ImageService {
-    private final AmazonS3Client amazonS3Client;
+    private final AmazonS3 amazonS3;
     // todo 파일 확장자, 파일 크기 설정
     @Value("${aws.bucket}")
     private String bucket;
 
-    public String upload(MultipartFile multipartFile) throws IOException {
+    public String upload(MultipartFile multipartFile) {
         String originalFilename = multipartFile.getOriginalFilename();
 
         ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentType(multipartFile.getContentType());
         objectMetadata.setContentLength(multipartFile.getSize());
 
-        amazonS3Client.putObject(bucket, originalFilename, multipartFile.getInputStream(), objectMetadata);
-        return amazonS3Client.getUrl(bucket, originalFilename).toString();
+        try {
+            amazonS3.putObject(bucket, originalFilename, multipartFile.getInputStream(), objectMetadata);
+        } catch (IOException e) {
+            throw new RuntimeException("파일 업로드 실패", e);
+        }
+        return amazonS3.getUrl(bucket, originalFilename).toString();
 
     }
 }
